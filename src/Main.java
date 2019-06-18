3e6fe26150b8e328d6a72cd03b3a747ed2bf12a6
Link <sandlincoln@gmail.com>
2019-06-18 00:58:16 -0700

    This is my bookmark sorter. I might optimize later.

---
+++ src/Main.java
@@ -0,0 +1,112 @@
+import java.io.Writer;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private final String[] keywords = {"haskell", "perl", "rust"};
+    private String[] keywordFileNames = new String[keywords.length];
+    private File[] keywordFiles = new File[keywords.length];
+    private File elseFile;
+    private final String elseFileName = "src/else.html";
+
+    private Scanner questionScanner;
+    {
+        try {
+            questionScanner = new Scanner(new File("src/input.html"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    private Main(){
+        try {
+            fileCreator();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //System.out.println("keyline , " + "line , " + stringChecker("keyline", "line"));
+
+        System.out.println();
+
+        readerAndWriter();
+    }
+
+    private void readerAndWriter(){
+        String line;
+        int lineNumber = 1;
+        boolean fitsKeyword = false;
+        do {
+            fitsKeyword = false;
+            line = questionScanner.nextLine();
+            //System.out.println(line + " at " + lineNumber);
+            ++lineNumber;
+            for(int x1 = 0; x1 < keywords.length; ++x1){
+                //System.out.println(line +" : " + keywords[x1] + " : " + stringChecker(line, keywords[x1]));
+                if(stringChecker(line, keywords[x1])){
+                    Writer out;
+                    try {
+                        out = new FileWriter(keywordFileNames[x1], true);
+                        out.write(line + '\n'); //Write the string to the file
+                        out.flush();
+                        out.close(); //Close the file
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    fitsKeyword = true;
+                    break;
+                }
+            }
+            if(!fitsKeyword){
+                Writer out;
+                try {
+                    out = new FileWriter(elseFile, true);
+                    out.write(line + '\n'); //Write the string to the file
+                    out.flush();
+                    out.close(); //Close the file
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }while(!line.equals("</html>"));
+    }
+
+    private boolean stringChecker(String line, String keyword){
+        return (line.toUpperCase()).contains(keyword.toUpperCase()); //keyword in line;
+    }
+
+    private void fileCreator() throws IOException {
+        String keywordFileName;
+        for(int x1 = 0; x1 < keywords.length; ++x1){
+            keywordFileName = "src/" + keywords[x1] + ".html";
+            File file = new File(keywordFileName);
+            if(file.createNewFile()){
+                System.out.println(keywordFileName + " File Created");
+            }
+            else{
+                System.out.println("File " + keywordFileName + " already exists");
+            }
+            keywordFiles[x1] = file;
+            keywordFileNames[x1] = keywordFileName;
+            //System.out.println("File: " + keywordFileName);
+        }
+
+        elseFile = new File(elseFileName);
+        if(elseFile.createNewFile()){
+            System.out.println(elseFileName + " File Created");
+        }
+        else{
+            System.out.println("File " + elseFileName + " already exists");
+        }
+    }
+
+}
